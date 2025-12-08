7ee9ac9103c58d140aa8e3ee1df85497075acffd
Atig-Hamza <hamzaatig@icloud.com>
2025-12-08 12:18:20 +0100

    feat: implement routing with react-router-dom in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
 
 function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
