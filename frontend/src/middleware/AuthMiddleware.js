445e95597b93a1abb35d9aa55e69383d27a3e2db
Atig-Hamza <hamzaatig@icloud.com>
2025-12-09 14:11:06 +0100

    feat: add AuthMiddleware component for token validation and navigation

---
+++ frontend/src/middleware/AuthMiddleware.js
@@ -0,0 +1,37 @@
+import React, { use, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+export default function AuthMiddleware({children}) {
+    const navigate = useNavigate();
+    const [isValid, setIsValid] = useState(false);
+    useEffect(() => {
+        async function validateToken() {
+            const token = localStorage.getItem('token');
+            console.log('Token from localStorage:', token);
+            if (!token) {
+                navigate('/login');
+                return;
+            }
+            try {
+                const response = await axios.post('http://localhost:4000/api/auth/validate-token', {
+                    token
+                });
+                console.log('Token validation response:', response.data);
+                console.log('children:', children);
+                setIsValid(true);
+                console.log('Content set to children:', children);
+            } catch (error) {
+                console.error('Token validation error:', error);
+                navigate('/login');
+            }
+        };
+        validateToken()
+    }, []);
+    if(!isValid){
+        return null;
+    }
+    return (
+        <>{children}</>
+    )
+}
