e1f8f5327d9af2fe653a18ba322ed79e484f7066
Atig-Hamza <hamzaatig@icloud.com>
2025-12-09 14:08:30 +0100

    feat: configure CORS middleware for handling cross-origin requests

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import dataBase from './src/config/db.js';
 import router from './src/routes/index.js';
+import CORS from 'cors';
 
 const app = express();
 const PORT = 4000;
+
+app.use(CORS(
+    {origin: 'http://localhost:5173', credentials: true}
+));
 
 dataBase();
 
