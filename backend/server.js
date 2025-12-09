b4e20a7e3f9a020b552a87497d3ca18de7b3c0e0
Atig-Hamza <hamzaatig@icloud.com>
2025-12-09 12:09:09 +0100

    feat: set up express middleware for JSON parsing and API routing

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import dataBase from './src/config/db.js';
+import router from './src/routes/index.js';
 
 const app = express();
 const PORT = 4000;
 
 dataBase();
+
+app.use(express.json());
+
+app.use('/api', router);
 
 app.listen(PORT, ()=>{
     console.log(`server is runned on http://localhost:${PORT}`)
