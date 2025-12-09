14da9b72cdaebeef245919d108141eb5a09979e3
Atig-Hamza <hamzaatig@icloud.com>
2025-12-09 14:10:45 +0100

    feat: add validate-token route to Auth controller for token verification

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -4,5 +4,6 @@ import * as AuthController from '../controllers/Auth.controller.js';
 const router = express.Router();
 
 router.post('/login', AuthController.Login);
+router.post('/validate-token', AuthController.ValidateToken);
 
 export default router;
