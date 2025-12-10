440e919156901547a7535329ee1f5bfd9765cd36
Atig-Hamza <hamzaatig@icloud.com>
2025-12-10 16:17:03 +0100

    feat: create Applications page with Sidebar component integration

---
+++ frontend/src/Pages/Admin/Application.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Sidebar from '../../components/SideBare';
+
+const Applications = () => {
+    return (
+        <div className="min-h-screen bg-[#FAFAFA] flex">
+            <Sidebar />
+            <div className="flex-1 ml-[280px]">
+                
+            </div>
+        </div>
+    );
+};
+
+export default Applications;
