f0d35b709f761ebcf5395fcdefd0a4b412e05556
Atig-Hamza <hamzaatig@icloud.com>
2025-12-11 14:13:33 +0100

    fix: adjust formatting in admin dashboard route for consistency

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
-        <Route path="/admin/dashboard" element={<AuthMiddleware><AdminDashboard /></AuthMiddleware>} />
+        <Route path="/admin/dashboard" element={<AuthMiddleware><AdminDashboard /></AuthMiddleware>}  />
         <Route path="/admin/applications" element={<AuthMiddleware><Applications /></AuthMiddleware>} />
         <Route path="/driver/dashboard" element={<AuthMiddleware><DriverDashboard /></AuthMiddleware>} />
       </Routes>
