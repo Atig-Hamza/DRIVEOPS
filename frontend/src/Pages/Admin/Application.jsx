67b8b56cf7880fc63b80c21f04f7f2de5b91e9db
Atig-Hamza <hamzaatig@icloud.com>
2025-12-11 13:41:09 +0100

    feat: add ApplicationDash component to Applications page

--- frontend/src/Pages/Admin/Application.jsx
+++ frontend/src/Pages/Admin/Application.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Sidebar from '../../components/SideBare';
+import ApplicationDash from '../../components/ApplicationDash';
 
 const Applications = () => {
     return (
         <div className="min-h-screen bg-[#FAFAFA] flex">
             <Sidebar />
             <div className="flex-1 ml-[280px]">
-                
+                <ApplicationDash />
             </div>
         </div>
     );
