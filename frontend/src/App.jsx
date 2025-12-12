94fd001d3c273c52f4c48bf46c15ee275cc4aeeb
Atig-Hamza <hamzaatig@icloud.com>
2025-12-12 15:19:58 +0100

    feat: add route for Trips in admin dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import DriverDashboard from './Pages/Driver/Dashboard';
 import AuthMiddleware from './middleware/AuthMiddleware.jsx';
 import Applications from './Pages/Admin/Application';
 import Trucks from './Pages/Admin/Trucks';
+import Trips from './Pages/Admin/Trips';
 import Traking from './Pages/Admin/Tracking.jsx';
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
         <Route path="/admin/dashboard" element={<AuthMiddleware><AdminDashboard /></AuthMiddleware>}  />
         <Route path="/admin/applications" element={<AuthMiddleware><Applications /></AuthMiddleware>} />
         <Route path="/admin/trucks" element={<AuthMiddleware><Trucks /></AuthMiddleware>} />
+        <Route path="/admin/trip" element={<AuthMiddleware><Trips /></AuthMiddleware>} />
         <Route path="/admin/tracking" element={<AuthMiddleware><Traking /></AuthMiddleware>} />
         <Route path="/driver/dashboard" element={<AuthMiddleware><DriverDashboard /></AuthMiddleware>} />
       </Routes>
