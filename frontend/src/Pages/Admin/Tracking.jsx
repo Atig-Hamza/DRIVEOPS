f2f1ceccbfcaf94e2a9ce5c6ee882b76b63fecd7
Atig-Hamza <hamzaatig@icloud.com>
2025-12-12 15:08:19 +0100

    feat: add Tracking component for Admin pages

---
+++ frontend/src/Pages/Admin/Tracking.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from '../../components/SideBare';
+
+
+function Traking(){
+    return(
+        <div>
+            <Sidebar />
+            <h1>Traking Page</h1>
+        </div>
+    )
+}
+
+export default Traking;
