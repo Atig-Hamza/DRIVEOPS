e193f7ef08e87f1168e2a9cede2e4e7878971957
Atig-Hamza <hamzaatig@icloud.com>
2025-12-12 15:07:48 +0100

    feat: add route for tier management

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import authRoutes from './auth.routes.js';
 import applicationRoutes from './application.routes.js';
 import truckRoutes from './truck.routes.js';
+import tierRoutes from './tier.routes.js';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/applications', applicationRoutes);
 router.use('/trucks', truckRoutes);
+router.use('/tiers', tierRoutes);
 
 export default router;
